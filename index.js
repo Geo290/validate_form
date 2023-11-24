0e98cf0c2c5f835a7af41d3108c489589577e2f4
Georgie <jorgeemmanuel.2003@gmail.com>
2023-11-23 19:34:22 -0600

    Feat: captcha div position modified

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var flag = flase;
+var flag = false;
 function validate() {
+    var user = document.getElementById("user").value
+    var password = document.getElementById("password").value
+
+    alert(user)
+    alert(password)
+
     if (flag) {
         console.log("Validation completed");
-        var user = document.getElementById("user").value
-        var password = document.getElementById("password").value
-
-        alert(user)
-        alert(password)
     }
 }
 
